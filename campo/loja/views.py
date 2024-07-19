4175d6b58192db70339dadcc9b9abcdf346c819e
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2024-07-19 19:21:15 +0000

    atualizado

--- campo/loja/views.py
+++ campo/loja/views.py
@@ -4,6 +4,18 @@ from .models import Produto, Fornecedor
 from django.db.models import Q 
 
 def pesquisar(request):
+
+
+    if request.method == "POST":
+        pesquisa = request.POST.get('pesquisa')
+        resultado = Produto.objects.filter(nome__contains=pesquisa) 
+        return render(request,'loja/pesquisa.html',{'nome': 'Thiago', 'resultado':resultado})
+
+
+
+
+
+
     resultado = Produto.objects.all()          #filter(nome="Tapioca")
     #return HttpResponse(resultado)   #(nome__contains=banana, preco=20, quantidade__gt=0)
     return render(request,'loja/pesquisa.html',{'nome': 'Thiago', 'resultado':resultado})
