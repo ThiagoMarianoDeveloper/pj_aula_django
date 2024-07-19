a0ccf37f928fa67b219b2c874c6c6fadfd108d16
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2024-07-19 17:50:42 +0000

    ffff

--- campo/loja/views.py
+++ campo/loja/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Produto, Fornecedor
+from django.db.models import Q 
 
 def pesquisar(request):
-    resultado = Produto.objects.filter(nome="Tapioca")
-    return HttpResponse(resultado)   #(nome__contains=banana, preco=20, quantidade__gt=0)
+    resultado = Produto.objects.all()          #filter(nome="Tapioca")
+    #return HttpResponse(resultado)   #(nome__contains=banana, preco=20, quantidade__gt=0)
+    return render(request,'loja/pesquisa.html',{'nome': 'Thiago', 'resultado':resultado})
 
 def fornecedor(request):
     resultado = Fornecedor.objects.all()
